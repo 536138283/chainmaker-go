18c52ba6a3f0cc2a4f62b5835da181899c85c860
liuzhiwang <lzwisbadbad@163.com>
2021-06-25 20:48:30 +0800

    style:add log

--- module/core/abft/merger.go
+++ module/core/abft/merger.go
@@ -119,7 +119,7 @@ func (m *Merger) doMerge(
 		// update allTxsMap
 		m.allTxsMap[txId] = tx
 	}
-	m.log.Debugf("branchId[%s], repeatTx[%d]", hex.EncodeToString(txBatch.Header.BlockHash), repeatTx)
+	m.log.Debugf("height[%d], branchId[%s], repeatTx[%d]", baseTxBatch.Header.BlockHeight, hex.EncodeToString(txBatch.Header.BlockHash), repeatTx)
 }
 
 func (m *Merger) getRepeatTx(txBatchID string) ([]int, map[string]struct{}) {
