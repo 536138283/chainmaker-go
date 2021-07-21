6393c5de46b726b7c0d6059a43857bb03be865e3
mosesyzding <893588380@qq.com>
2021-07-21 14:52:19 +0800

    add docker_scheduler_test.go

---
+++ module/vm/docker-go/dockercontainer/module/core/docker_scheduler_test.go
@@ -0,0 +1,44 @@
+/*
+Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package core
+
+import (
+	"chainmaker.org/chainmaker-go/docker-go/dockercontainer/pb/protogo"
+	"testing"
+)
+
+func getBlankDockerScheduler() *DockerScheduler{
+	userController := &UsersManager{}
+	handlerRegister := &HandlerRegister{}
+	return NewDockerScheduler(userController, handlerRegister)
+}
+
+//func (s *DockerScheduler) RegisterResponseCh(txId string, responseCh chan *protogo.CDMMessage) {
+func TestRegisterResponseCh(t *testing.T) {
+	s := getBlankDockerScheduler()
+	txId := "txId1"
+	responseChan := make(chan *protogo.CDMMessage)
+	s.RegisterResponseCh(txId, responseChan)
+	t.Run("test1", func(t *testing.T) {
+		if s.getStateResponseChMap[txId] != responseChan {
+			t.Errorf("test RegisterResponseCh failed.")
+		}
+	})
+}
+
+//func (s *DockerScheduler) GetResponseChByTxId(txId string) chan *protogo.CDMMessage {
+func TestGetResponseChByTxId(t *testing.T) {
+	s := getBlankDockerScheduler()
+	txId := "txId1"
+	responseChan := make(chan *protogo.CDMMessage)
+	s.RegisterResponseCh(txId, responseChan)
+	t.Run("test1", func(t *testing.T) {
+		if s.GetResponseChByTxId(txId) != responseChan {
+			t.Errorf("test RegisterResponseCh failed.")
+		}
+	})
+}
