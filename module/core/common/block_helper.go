d805c82e71a1edc0e715b4b1b4af4c4f1d17f39a
liuzhiwang <lzwisbadbad@163.com>
2021-08-20 16:46:26 +0800

    fix:fix bug#1002036 & bug#1002097

--- module/core/common/block_helper.go
+++ module/core/common/block_helper.go
@@ -11,7 +11,6 @@ import (
 	"chainmaker.org/chainmaker-go/common/crypto/hash"
 	commonErrors "chainmaker.org/chainmaker-go/common/errors"
 	"chainmaker.org/chainmaker-go/common/msgbus"
-	"chainmaker.org/chainmaker-go/core/common/scheduler"
 	"chainmaker.org/chainmaker-go/core/provider/conf"
 	"chainmaker.org/chainmaker-go/localconf"
 	"chainmaker.org/chainmaker-go/monitor"
@@ -425,6 +424,7 @@ type VerifierBlockConf struct {
 	BlockchainStore protocol.BlockchainStore
 	ProposalCache   protocol.ProposalCache // proposal cache
 	StoreHelper     conf.StoreHelper
+	TxScheduler     protocol.TxScheduler
 }
 
 type VerifierBlock struct {
@@ -439,6 +439,7 @@ type VerifierBlock struct {
 	blockchainStore protocol.BlockchainStore
 	proposalCache   protocol.ProposalCache // proposal cache
 	storeHelper     conf.StoreHelper
+
 }
 
 func NewVerifierBlock(conf *VerifierBlockConf) *VerifierBlock {
@@ -453,9 +454,8 @@ func NewVerifierBlock(conf *VerifierBlockConf) *VerifierBlock {
 		blockchainStore: conf.BlockchainStore,
 		proposalCache:   conf.ProposalCache,
 		storeHelper:     conf.StoreHelper,
+		txScheduler:     conf.TxScheduler,
 	}
-	var schedulerFactory scheduler.TxSchedulerFactory
-	verifyBlock.txScheduler = schedulerFactory.NewTxScheduler(verifyBlock.vmMgr, verifyBlock.chainConf, conf.StoreHelper)
 	return verifyBlock
 }
 
@@ -729,10 +729,11 @@ func (chain *BlockCommitterImpl) AddBlock(block *commonpb.Block) (err error) {
 		return err
 	}
 	lastProposed, rwSetMap, conEventMap := chain.proposalCache.GetProposedBlock(block)
-	if err = chain.checkLastProposedBlock(block, lastProposed, err, height, rwSetMap, conEventMap); err != nil {
-		return err
+	if lastProposed == nil {
+		if err, lastProposed, rwSetMap, conEventMap = chain.checkLastProposedBlock(block); err != nil {
+			return err
+		}
 	}
-
 	checkLasts := utils.CurrentTimeMillisSeconds() - startTick
 
 	dbLasts, snapshotLasts, confLasts, otherLasts, pubEvent, err := chain.commonCommit.CommitBlock(block, rwSetMap, conEventMap)
@@ -779,20 +780,18 @@ func (chain *BlockCommitterImpl) syncWithTxPool(block *commonpb.Block, height in
 	return txRetry
 }
 
-func (chain *BlockCommitterImpl) checkLastProposedBlock(block *commonpb.Block, lastProposed *commonpb.Block,
-	err error, height int64, rwSetMap map[string]*commonpb.TxRWSet, conEventMap map[string][]*commonpb.ContractEvent) error {
-	if lastProposed != nil {
-		return nil
-	}
-	err = chain.verifier.VerifyBlock(block, protocol.SYNC_VERIFY)
+func (chain *BlockCommitterImpl) checkLastProposedBlock(block *commonpb.Block) (
+	error, *commonpb.Block, map[string]*commonpb.TxRWSet, map[string][]*commonpb.ContractEvent) {
+	err := chain.verifier.VerifyBlock(block, protocol.SYNC_VERIFY)
 	if err != nil {
-		chain.log.Error("block verify failed [%d](hash:%x), %s", height, block.Header.BlockHash, err)
-		return err
+		chain.log.Error("block verify failed [%d](hash:%x), %s", block.Header.BlockHeight, block.Header.BlockHash, err)
+		return err, nil, nil, nil
 	}
-	lastProposed, rwSetMap, conEventMap = chain.proposalCache.GetProposedBlock(block)
+
+	lastProposed, rwSetMap, conEventMap := chain.proposalCache.GetProposedBlock(block)
 	if lastProposed == nil {
-		chain.log.Error("block not verified [%d](hash:%x)", height, block.Header.BlockHash)
-		return fmt.Errorf("block not verified [%d](hash:%x)", height, block.Header.BlockHash)
+		chain.log.Error("block not verified [%d](hash:%x)", block.Header.BlockHeight, block.Header.BlockHash)
+		return fmt.Errorf("block not verified [%d](hash:%x)", block.Header.BlockHeight, block.Header.BlockHash), lastProposed, rwSetMap, conEventMap
 	}
-	return nil
+	return nil, lastProposed, rwSetMap, conEventMap
 }
