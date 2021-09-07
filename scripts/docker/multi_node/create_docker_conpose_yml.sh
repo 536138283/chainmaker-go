2b9e2247bc8a0d669254a6dcc6d07dcd6353e940
taifu yuan <515789026@qq.com>
2021-09-07 21:20:33 +0800

    script: 生成时，连同步节点一起生成。docker大量脚本

--- scripts/docker/multi_node/create_docker_conpose_yml.sh
+++ scripts/docker/multi_node/create_docker_conpose_yml.sh
@@ -1,5 +1,6 @@
-P2P_PORT_PREFIX=$1
-RPC_PORT_PREFIX=$2
+
+P2P_PORT=$1
+RPC_PORT=$2
 NODE_COUNT=$3
 CONFIG_DIR=$4
 SERVER_COUNT=$5
@@ -11,9 +12,9 @@ TEMPLATE_FILE="tpl_docker-compose_services.yml"
 
 function show_help() {
     echo "Usage:  "
-    echo "  create_yml.sh p2p_port_prefix(default:11300) rpc_port_prefix(default:12300) node_count config_dir(relative current dir or absolutely dir) server_count(default:1)"
-    echo "    eg: ./create_docker_conpose_yml.sh 11300 12300 20 ../../../build/config 10"
-    echo "    eg: ./create_docker_conpose_yml.sh 11300 12300 20 /mnt/d/develop/workspace/go/chainmaker-go/build/config 10"
+    echo "  create_yml.sh P2P_PORT(default:11301) RPC_PORT(default:12301) node_count config_dir(relative current dir or absolutely dir) server_count(default:1)"
+    echo "    eg: ./create_docker_conpose_yml.sh 11301 12301 20 ../../../build/config 10"
+    echo "    eg: ./create_docker_conpose_yml.sh 11301 12301 20 /mnt/d/develop/workspace/go/chainmaker-go/build/config 10"
 }
 if [ ! $# -eq 2 ] && [ ! $# -eq 3 ] && [ ! $# -eq 4 ] && [ ! $# -eq 5 ]; then
     echo "invalid params"
@@ -32,24 +33,24 @@ function xsed() {
 }
 
 function check_params() {
-    if  [[ ! -n $P2P_PORT_PREFIX ]] ;then
+    if  [[ ! -n $P2P_PORT ]] ;then
         show_help
         exit 1
     fi
 
-    if  [ ${P2P_PORT_PREFIX} -ge 60000 ] || [ ${P2P_PORT_PREFIX} -le 10000 ];then
-        echo "p2p_port_prefix should >=10000 && <=60000"
+    if  [ ${P2P_PORT} -ge 60000 ] || [ ${P2P_PORT} -le 10000 ];then
+        echo "P2P_PORT should >=10000 && <=60000"
         show_help
         exit 1
     fi
 
-    if  [[ ! -n $RPC_PORT_PREFIX ]] ;then
+    if  [[ ! -n $RPC_PORT ]] ;then
         show_help
         exit 1
     fi
 
-    if  [ ${RPC_PORT_PREFIX} -ge 60000 ] || [ ${RPC_PORT_PREFIX} -le 10000 ];then
-        echo "rpc_port_prefix should >=10000 && <=60000"
+    if  [ ${RPC_PORT} -ge 60000 ] || [ ${RPC_PORT} -le 10000 ];then
+        echo "RPC_PORT should >=10000 && <=60000"
         show_help
         exit 1
     fi
@@ -90,8 +91,6 @@ function generate_yml_file() {
       echo -e "version: '3'\n"  >> $current_config_file
       echo -e "services:"  >> $current_config_file
     fi
-    P2P_PORT_PREFIX=$(($P2P_PORT_PREFIX+1))
-    RPC_PORT_PREFIX=$(($RPC_PORT_PREFIX+1))
     if [ ! -f $tmp_file ];then
       cp $TEMPLATE_FILE $tmp_file
     fi
@@ -99,10 +98,12 @@ function generate_yml_file() {
     xsed "s%{config_dir}%${node_config_dir}%g" $tmp_file
     xsed "s%{id}%${k}%g" $tmp_file
     xsed "s%{image}%${IMAGE}%g" $tmp_file
-    xsed "s%{rpc_port}%${RPC_PORT_PREFIX}%g" $tmp_file
-    xsed "s%{p2p_port}%${P2P_PORT_PREFIX}%g" $tmp_file
+    xsed "s%{rpc_port}%${RPC_PORT}%g" $tmp_file
+    xsed "s%{p2p_port}%${P2P_PORT}%g" $tmp_file
     cat $tmp_file >> $current_config_file
     rm -f $tmp_file
+    P2P_PORT=$(($P2P_PORT+1))
+    RPC_PORT=$(($RPC_PORT+1))
   done
 
 }
