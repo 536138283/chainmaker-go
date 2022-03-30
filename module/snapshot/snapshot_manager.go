2259934600569174b18dc27001b2f3d742008d06
liuzhiwang <lzwisbadbad@163.com>
2022-03-30 14:57:52 +0800

    fix log's error

--- module/snapshot/snapshot_manager.go
+++ module/snapshot/snapshot_manager.go
@@ -86,7 +86,7 @@ func (m *ManagerImpl) NotifyBlockCommitted(block *commonPb.Block) error {
 
 		if block.Header.BlockHeight-snapshot.GetBlockHeight() > 8 {
 			delete(m.snapshots, finger)
-			log.Infof("delete snapshot %v at height %d while gc", deleteFp, snapshot.blockHeight)
+			log.Infof("delete snapshot %v at height %d while gc", finger, snapshot.blockHeight)
 			snapshot.SetPreSnapshot(nil)
 		}
 	}
