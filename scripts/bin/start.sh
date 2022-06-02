8281896460dbb4a4b497c8d9078276ebd909cd8a
litenghui <644052732@qq.com>
2022-06-02 21:48:02 +0800

    refactoring scripts for vm-docker-go

--- scripts/bin/start.sh
+++ scripts/bin/start.sh
@@ -10,10 +10,31 @@ export LD_LIBRARY_PATH=$(dirname $PWD)/lib:$LD_LIBRARY_PATH
 export PATH=$(dirname $PWD)/lib:$PATH
 export WASMER_BACKTRACE=1
 
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
 config_file="../config/{org_id}/chainmaker.yml"
+# config_file="../../config/wx-org1-solo/chainmaker.yml"
 
 # if clean existed container(can be -y/-f/force)
 FORCE_CLEAN=$1
+
+eval $(parse_yaml "$config_file" "chainmaker_")
+
 
 # if enable docker vm service and use unix domain socket, run a vm docker container
 start_docker_vm() {
@@ -45,8 +66,8 @@ start_docker_vm() {
   fi
 
   # concat mount_path and log_path for container to mount
-  mount_path=$(grep dockervm_mount_path $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
-  log_path=$(grep dockervm_log_path $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
+  mount_path=$chainmaker_vm_dockervm_mount_path
+  log_path=$chainmaker_vm_dockervm_log_path
   if [[ "${mount_path:0:1}" != "/" ]];then
     mount_path=$(pwd)/$mount_path
   fi
@@ -57,12 +78,47 @@ start_docker_vm() {
   mkdir -p "$mount_path"
   mkdir -p "$log_path"
 
-  echo "start docker vm service container:"
-  docker run -itd \
-    -v "$mount_path":/mount \
-    -v "$log_path":/log \
-    --name DOCKERVM-{org_id} \
-    --privileged $image_name
+
+  enable_docker_vm=$chainmaker_vm_enable_dockervm
+  enable_uds=$chainmaker_vm_uds_open
+  start_now=$chainmaker_vm_start_now
+  if [[ $enable_docker_vm = "true" &&  $start_now != "false" ]]
+  then
+    if [[ $enable_uds = "true" ]]
+    then
+      echo "docker vm protocol: unix domain socket"
+
+      docker run -itd \
+      -e CHAIN_RPC_PROTOCOL="0" \
+      -v "$mount_path":/mount \
+      -v "$log_path":/log \
+      --name DOCKERVM-{org_id} \
+      --privileged $image_name
+    else
+      # $enable_uds = "false"
+      echo "docker vm protocol: tcp"
+        runtime_server_port=$chainmaker_vm_runtime_server_port
+        contract_engine_port=$chainmaker_vm_contract_engine_port
+        contract_engine_timeout=$chainmaker_vm_contract_engine_dial_timeout
+        contract_engine_max_send_size=$chainmaker_vm_contract_engine_max_send_msg_size
+        contract_engine_max_recv_size=$chainmaker_vm_contract_engine_max_recv_msg_size
+
+        EXPOSE_PORT=$contract_engine_port
+
+        docker run -itd \
+        -v "$mount_path":/mount \
+        -v "$log_path":/log \
+        -p "$EXPOSE_PORT":"$EXPOSE_PORT" \
+        -e CHAIN_RPC_PROTOCOL="1" \
+        -e CHAIN_RPC_PORT="$contract_engine_port" \
+        -e SANDBOX_RPC_PORT="$runtime_server_port" \
+        -e MAX_SEND_MSG_SIZE="$contract_engine_max_send_size" \
+        -e MAX_RECV_MSG_SIZE="$contract_engine_max_recv_size" \
+        -e MAX_CONN_TIMEOUT="$contract_engine_timeout" \
+        --name DOCKERVM-{org_id} \
+        --privileged $image_name
+    fi
+  fi
 
   retval="$?"
   if [ $retval -ne 0 ]; then
@@ -75,13 +131,14 @@ start_docker_vm() {
 }
 
 
-pid=`ps -ef | grep chainmaker | grep "\-c ../config/{org_id}/chainmaker.yml" | grep -v grep |  awk  '{print $2}'`
-if [ -z ${pid} ];then
+pid=$(ps -ef | grep chainmaker | grep "\-c ../config/{org_id}/chainmaker.yml" | grep -v grep |  awk  '{print $2}')
+if [ -z "${pid}" ];then
 
     # check if need to start docker vm service.
-    enable_docker_vm=$(grep enable_dockervm $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
-    enable_uds=$(grep uds_open $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
-    if [[ $enable_docker_vm = "true" && $enable_uds = "true" ]]
+    enable_docker_vm=$chainmaker_vm_enable_dockervm
+    enable_uds=$chainmaker_vm_uds_open
+    start_now=$chainmaker_vm_start_now
+    if [[ $enable_docker_vm = "true" && $start_now != "false" ]]
     then
       start_docker_vm
     fi
@@ -93,4 +150,3 @@ if [ -z ${pid} ];then
 else
     echo "chainmaker is already started"
 fi
-
--- scripts/bin/stop.sh
+++ scripts/bin/stop.sh
@@ -6,30 +6,31 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-pid=`ps -ef | grep chainmaker | grep "\-c ../config/{org_id}/chainmaker.yml" | grep -v grep |  awk  '{print $2}'`
-if [ ! -z ${pid} ];then
-    kill $pid
-    echo "chainmaker is stopping..."
-fi
+# pid=`ps -ef | grep chainmaker | grep "\-c ../config/{org_id}/chainmaker.yml" | grep -v grep |  awk  '{print $2}'`
+# if [ ! -z ${pid} ];then
+#     kill $pid
+#     echo "chainmaker is stopping..."
+# fi
 
 # if enable docker vm service and use unix domain socket, stop the running container
 stop_docker_vm() {
-  config_file="../config/{org_id}/chainmaker.yml"
+  # config_file="../config/{org_id}/chainmaker.yml"
+  config_file="../../config/wx-org1-solo/chainmaker.yml"
   enable_docker_vm=$(grep enable_dockervm $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
-  enable_uds=$(grep uds_open $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
-  container_name=DOCKERVM-{org_id}
-  if [[ $enable_docker_vm = "true" && $enable_uds = "true" ]]
+  start_now=$(grep start_now $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
+  # container_name=DOCKERVM-{org_id}
+  container_name=DOCKERVM-solo
+  if [[ $enable_docker_vm = "true" && $start_now != "false" ]]
   then
     container_exists=$(docker ps -f name="$container_name" --format '{{.Names}}')
     if [[ $container_exists ]]; then
-      echo "stop docker vm container: $container_name"
+      echo "stop docker vm container:"
       docker stop "$container_name"
     fi
   fi
 }
-stop_docker_vm
 
-if [ ! -z ${pid} ];then
-    lsof -p $pid +r 1 &>/dev/null
+if [ ! -z "${pid}" ];then
+    lsof -p "$pid" +r 1 &>/dev/null
 fi
 echo "chainmaker is stopped"
