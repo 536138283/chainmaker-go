e70a4459f37f6a4bf6ac72de8d2877994e00e240
litenghui <644052732@qq.com>
2022-06-08 11:16:30 +0800

    fix stop.sh

--- scripts/bin/stop.sh
+++ scripts/bin/stop.sh
@@ -6,29 +6,30 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# pid=`ps -ef | grep chainmaker | grep "\-c ../config/{org_id}/chainmaker.yml" | grep -v grep |  awk  '{print $2}'`
-# if [ ! -z ${pid} ];then
-#     kill $pid
-#     echo "chainmaker is stopping..."
-# fi
+pid=`ps -ef | grep chainmaker | grep "\-c ../config/{org_id}/chainmaker.yml" | grep -v grep |  awk  '{print $2}'`
+if [ ! -z ${pid} ];then
+    kill $pid
+    echo "chainmaker is stopping..."
+fi
 
 # if enable docker vm service and use unix domain socket, stop the running container
 stop_docker_vm() {
-  # config_file="../config/{org_id}/chainmaker.yml"
-  config_file="../../config/wx-org1-solo/chainmaker.yml"
+  config_file="../config/{org_id}/chainmaker.yml"
   enable_docker_vm=$(grep enable_dockervm $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
   start_now=$(grep start_now $config_file | awk -F: '{gsub(/ /, "", $2);print $2}')
-  # container_name=DOCKERVM-{org_id}
-  container_name=DOCKERVM-solo
+  container_name=DOCKERVM-{org_id}
   if [[ $enable_docker_vm = "true" && $start_now != "false" ]]
   then
     container_exists=$(docker ps -f name="$container_name" --format '{{.Names}}')
+
     if [[ $container_exists ]]; then
       echo "stop docker vm container:"
       docker stop "$container_name"
     fi
   fi
 }
+
+stop_docker_vm
 
 if [ ! -z "${pid}" ];then
     lsof -p "$pid" +r 1 &>/dev/null
