8e51bc9bb6b7e3f5705bf2eecc174b7cd84f0b8d
DevinZeng <studyzy@qq.com>
2022-08-12 11:01:37 +0800

    test: Add more evm operation to test/scenario1_evm
    --story=1012022

--- test/scenario1_evm/getBalance.sh
+++ test/scenario1_evm/getBalance.sh
@@ -1,7 +1,16 @@
 cd $CMC
+echo "UserA balance:"
 ./cmc client contract user get \
 --contract-name=ERC20 \
 --abi-file-path=../../testdata/erc20.abi \
 --method=balanceOf \
 --sdk-conf-path=../config/sdk_config.yml \
 --params="[{\"address\":\"0x5a3e1a768e3ad7f49f7207c1cc113a337dfaa4ba\"}]"
+
+echo "UserB balance:"
+./cmc client contract user get \
+--contract-name=ERC20 \
+--abi-file-path=../../testdata/erc20.abi \
+--method=balanceOf \
+--sdk-conf-path=../config/sdk_config.yml \
+--params="[{\"address\":\"0xebe30584c91d648adadfb746d56c0d38dcb2d262\"}]"
