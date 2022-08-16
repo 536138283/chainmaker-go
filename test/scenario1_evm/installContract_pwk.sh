3f77319e8b0b4bbc0d88c55426e4d056548d223e
DevinZeng <studyzy@qq.com>
2022-08-16 16:20:15 +0800

    fix: Qta chain2 pwk admin key miss
    --story=0

--- test/scenario1_evm/installContract_pwk.sh
+++ test/scenario1_evm/installContract_pwk.sh
@@ -6,7 +6,7 @@ cd $CMC
 --byte-code-path=../../testdata/erc20.bin \
 --version=1.0 \
 --sdk-conf-path=../config/sdk_config.yml \
---admin-key-file-paths=../config/wx-org1.chainmaker.org/keys/user/admin1/admin1.key,../config/wx-org2.chainmaker.org/keys/user/admin1/admin1.key,../config/wx-org3.chainmaker.org/keys/user/admin1/admin1.key \
+--admin-key-file-paths=../config/wx-org1.chainmaker.org/keys/user/admin/admin.key,../config/wx-org2.chainmaker.org/keys/user/admin/admin.key,../config/wx-org3.chainmaker.org/keys/user/admin/admin.key \
 --admin-org-ids=wx-org1.chainmaker.org,wx-org2.chainmaker.org,wx-org3.chainmaker.org \
 --sync-result=true
 
