35367e82e65229ae9d68160a086144ea93069b4f
tedcxwang <tedcxwang@tencent.com>
2022-09-28 15:24:41 +0800

    feat: Merge develop
    --story=1011688

---
+++ module/consensus/cutover/consensus_switch_subscriber_test.go
@@ -0,0 +1,79 @@
+package cutover
+
+import (
+	"encoding/hex"
+	"fmt"
+	"testing"
+
+	"chainmaker.org/chainmaker/common/v2/msgbus"
+	"chainmaker.org/chainmaker/logger/v2"
+	"chainmaker.org/chainmaker/pb-go/v2/config"
+	"chainmaker.org/chainmaker/pb-go/v2/consensus"
+	"chainmaker.org/chainmaker/protocol/v2"
+	"github.com/gogo/protobuf/proto"
+)
+
+type testConsensusCarrier struct {
+}
+
+func (t *testConsensusCarrier) SwitchConsensus(c *config.ConsensusConfig) error {
+	fmt.Println("switch consensus to", c.Type.String())
+	return nil
+}
+
+func TestConsensusSwitchSubscriber_OnMessage(t *testing.T) {
+	type fields struct {
+		consensusCarrier ConsensusCarrier
+		consensusConfig  config.ConsensusConfig
+		log              protocol.Logger
+	}
+	type args struct {
+		msg *msgbus.Message
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "test",
+			fields: fields{
+				consensusCarrier: &testConsensusCarrier{},
+				consensusConfig: config.ConsensusConfig{
+					Type: consensus.ConsensusType_TBFT,
+				},
+				log: newMockLogger(),
+			},
+			args: args{
+				msg: &msgbus.Message{
+					Topic:   msgbus.ChainConfig,
+					Payload: genPayload(),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &ConsensusSwitchSubscriber{
+				consensusCarrier: tt.fields.consensusCarrier,
+				consensusConfig:  tt.fields.consensusConfig,
+				log:              tt.fields.log,
+			}
+			cs.OnMessage(tt.args.msg)
+		})
+	}
+}
+
+func newMockLogger() protocol.Logger {
+	return logger.GetLoggerByChain(logger.MODULE_CONSENSUS, "test_chain_id")
+}
+
+func genPayload() []string {
+	jsonBz, _ := proto.Marshal(&config.ChainConfig{
+		Consensus: &config.ConsensusConfig{
+			Type: consensus.ConsensusType_RAFT,
+		},
+	})
+	str := hex.EncodeToString(jsonBz)
+	return []string{str}
+}
---
+++ module/core/maxbftmode/committer/block_committer_impl.go
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package committer
+
+import (
+	"fmt"
+
+	"chainmaker.org/chainmaker/common/v2/msgbus"
+	"chainmaker.org/chainmaker/consensus-maxbft/v2/epoch"
+	commonPb "chainmaker.org/chainmaker/pb-go/v2/common"
+	"chainmaker.org/chainmaker/pb-go/v2/consensus"
+	"chainmaker.org/chainmaker/pb-go/v2/consensus/maxbft"
+	systemPb "chainmaker.org/chainmaker/pb-go/v2/syscontract"
+	"chainmaker.org/chainmaker/protocol/v2"
+	"github.com/gogo/protobuf/proto"
+)
+
+// BlockCommitter Decorator for the maxBft block commit feature
+type BlockCommitter struct {
+	conf     protocol.ChainConf
+	msgBus   msgbus.MessageBus
+	store    protocol.BlockchainStore
+	delegate protocol.BlockCommitter
+
+	epochStrategy string
+}
+
+// NewBLockCommitter new feature
+func NewBLockCommitter(delegate protocol.BlockCommitter,
+	conf protocol.ChainConf, msgBus msgbus.MessageBus, store protocol.BlockchainStore) (*BlockCommitter, error) {
+
+	bc := &BlockCommitter{delegate: delegate, conf: conf, msgBus: msgBus, store: store}
+	strategy, _, err := epoch.GetEpochStrategyFromConfig(conf.ChainConfig())
+	if err != nil {
+		return nil, err
+	}
+	bc.epochStrategy = strategy
+	return bc, nil
+}
+
+// AddBlock add block to db and do some things with maxbft
+func (bc *BlockCommitter) AddBlock(blk *commonPb.Block) error {
+	err := bc.delegate.AddBlock(blk)
+	if err != nil {
+		return err
+	}
+
+	// 其它策略模式下，当且仅当区块头的ConsensusArgs包含数据时，表示存在世代合约内容.
+	// 通知其它模块进行链配置更新
+	if len(blk.Header.ConsensusArgs) == 0 {
+		return nil
+	}
+	governance, err := bc.getGovernanceFromBlock(blk)
+	if err != nil {
+		err = fmt.Errorf("get governance from block failed. error: %+v", err)
+		return err
+	}
+	if governance != nil {
+		bc.msgBus.PublishSafe(msgbus.MaxbftEpochConf, governance)
+	}
+	return nil
+}
+
+func (bc *BlockCommitter) getGovernanceFromBlock(block *commonPb.Block) (*maxbft.GovernanceContract, error) {
+	var (
+		err  error
+		args = new(consensus.BlockHeaderConsensusArgs)
+	)
+	if err = proto.Unmarshal(block.Header.ConsensusArgs, args); err != nil {
+		err = fmt.Errorf("unmarshal consensus args failed. error:%+v", err)
+		return nil, err
+	}
+
+	// get the governanceContract from the txWrite
+	contractName := systemPb.SystemContract_GOVERNANCE.String()
+	if args.ConsensusData == nil || len(args.ConsensusData.TxWrites) == 0 ||
+		args.ConsensusData.TxWrites[0].ContractName != contractName {
+		// there is no governance contract information in the block, need not to switch epoch
+		return nil, nil
+	}
+
+	// get governance contract from the block, to get the configurations of the next epoch
+	governanceContract := new(maxbft.GovernanceContract)
+	if err = proto.Unmarshal(args.ConsensusData.TxWrites[0].GetValue(), governanceContract); err != nil {
+		err = fmt.Errorf("unmarshal txWrites value failed. error:%+v", err)
+		return nil, err
+	}
+	return governanceContract, nil
+}
