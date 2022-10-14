6ca4a0567e53972fca4801287d74d5c94bc2d2e9
yuan <515789026@qq.com>
2022-10-14 10:41:57 +0800

    refactor: prepare 脚本改造 --story=1012182

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,7 +6,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# check mac gun-getopt
+function checkEnv() {
+  getopt --test
+  if [ "$?" != "4" ];then
+    brew -v > /dev/null
+    if [ "$?" != "0" ];then
+      echo 'Please install brew for Mac: ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+    fi
+    echo 'Please install gnu-getopt for Mac: brew install gnu-getopt'
+    exit
+  fi
+}
+checkEnv
+
 set -e
+
+VERSION=v2.4.0_alpha
 
 NODE_CNT=$1
 CHAIN_CNT=$2
@@ -24,19 +40,28 @@ BUILD_CONFIG_PATH=${BUILD_PATH}/config
 CRYPTOGEN_TOOL_PATH=${PROJECT_PATH}/tools/chainmaker-cryptogen
 CRYPTOGEN_TOOL_BIN=${CRYPTOGEN_TOOL_PATH}/bin/chainmaker-cryptogen
 CRYPTOGEN_TOOL_CONF=${CRYPTOGEN_TOOL_PATH}/config/crypto_config_template.yml
-CRYPTOGEN_TOOL_TEST_KEYS=${CRYPTOGEN_TOOL_PATH}/config/test_keys.yml
+CRYPTOGEN_TOOL_PKCS11_KEYS=${CRYPTOGEN_TOOL_PATH}/config/pkcs11_keys.yml
 
-VERSION=2400
 
 BC_YML_TRUST_ROOT_LINE=$(awk '/trust roots list start/{print NR}' ${CONFIG_TPL_PATH}/chainconfig/bc_4_7.tpl)
 BC_YML_TRUST_ROOT_LINE_END=$(awk '/trust roots list end/{print NR}' ${CONFIG_TPL_PATH}/chainconfig/bc_4_7.tpl)
 
 function show_help() {
     echo "Usage:  "
-    echo "    prepare.sh node_cnt(1/4/7/10/13/16) chain_cnt(1-4) p2p_port(default:11301) rpc_port(default:12301) vm_go_runtime_port(default:32351) vm_go_engine_port(default:22351)"
+    echo "    prepare.sh node_cnt(1/4/7/10/13/16) chain_cnt(1-4)"
+    echo "               p2p_port(default:11301) rpc_port(default:12301)"
+    echo "               vm_go_runtime_port(default:32351) vm_go_engine_port(default:22351)"
+    echo "               -c consense-type: 1-TBFT,3-MAXBFT,4-RAFT "
+    echo "               -l log-level: DEBUG,INFO,WARN,ERROR"
+    echo "               -v docker-vm-enable: true,false"
+    echo "                  --vtp  vm go transport protocol: tcp,uds"
+    echo "                  --vlog vm go log level: DEBUG,INFO,WARN,ERROR"
+    echo "               -h show help"
     echo "    eg1: prepare.sh 4 1"
     echo "    eg2: prepare.sh 4 1 11301 12301"
     echo "    eg2: prepare.sh 4 1 11301 12301 32351 22351"
+    echo "    eg2: prepare.sh 4 1 11301 12301 32351 22351 -c 1 -l INFO -v true  --vtp=tcp --vlog=INFO"
+    echo "    eg2: prepare.sh 4 1 11301 12301 32351 22351 -c 1 -l INFO -v false --vtp=tcp --vlog=INFO"
 }
 
 if ( [ $# -eq 1 ] && [ "$1" ==  "-h" ] ) ; then
@@ -44,7 +69,7 @@ if ( [ $# -eq 1 ] && [ "$1" ==  "-h" ] ) ; then
     exit 1
 fi
 
-if [ ! $# -eq 2 ] && [ ! $# -eq 3 ] && [ ! $# -eq 4 ] && [ ! $# -eq 5 ] && [ ! $# -eq 6 ]; then
+if [ $# -eq 1 ]; then
     echo "invalid params"
     show_help
     exit 1
@@ -87,49 +112,47 @@ function check_params() {
         exit 1
     fi
 
-    if  [[ ! -n $P2P_PORT ]] ;then
+    # 判断是否是数字
+    if [ "$P2P_PORT" -gt 0 ] 2>/dev/null ;then
+      # 判断数字范围
+      if  [ ${P2P_PORT} -ge 60000 ] || [ ${P2P_PORT} -le 10000 ];then
+        P2P_PORT=11301
+      fi
+    else
         P2P_PORT=11301
     fi
+    echo "param P2P_PORT $P2P_PORT"
 
-    if  [ ${P2P_PORT} -ge 60000 ] || [ ${P2P_PORT} -le 10000 ];then
-        echo "p2p_port_prefix should >=10000 && <=60000"
-        show_help
-        exit 1
-    fi
-
-    if  [[ ! -n $RPC_PORT ]] ;then
+    if [ "$RPC_PORT" -gt 0 ] 2>/dev/null ;then
+      if  [ ${RPC_PORT} -ge 60000 ] || [ ${RPC_PORT} -le 10000 ];then
+        RPC_PORT=12301
+      fi
+    else
         RPC_PORT=12301
     fi
+    echo "param RPC_PORT $RPC_PORT"
 
-    if  [ ${RPC_PORT} -ge 60000 ] || [ ${RPC_PORT} -le 10000 ];then
-        echo "rpc_port_prefix should >=10000 && <=60000"
-        show_help
-        exit 1
-    fi
-
-    if  [[ ! -n $VM_GO_RUNTIME_PORT ]] ;then
+    if [ "$VM_GO_RUNTIME_PORT" -gt 0 ] 2>/dev/null ;then
+      if  [ ${VM_GO_RUNTIME_PORT} -ge 60000 ] || [ ${VM_GO_RUNTIME_PORT} -le 10000 ];then
+        VM_GO_RUNTIME_PORT=32351
+      fi
+    else
         VM_GO_RUNTIME_PORT=32351
     fi
+    echo "param VM_GO_RUNTIME_PORT $VM_GO_RUNTIME_PORT"
 
-    if  [ ${VM_GO_RUNTIME_PORT} -ge 60000 ] || [ ${VM_GO_RUNTIME_PORT} -le 10000 ];then
-        echo "vm_go_runtime_port should >=10000 && <=60000"
-        show_help
-        exit 1
-    fi
-
-    if  [[ ! -n $VM_GO_ENGINE_PORT ]] ;then
+    if [ "$VM_GO_ENGINE_PORT" -gt 0 ] 2>/dev/null ;then
+      if  [ ${VM_GO_ENGINE_PORT} -ge 60000 ] || [ ${VM_GO_ENGINE_PORT} -le 10000 ];then
+        VM_GO_ENGINE_PORT=22351
+      fi
+    else
         VM_GO_ENGINE_PORT=22351
     fi
-
-    if  [ ${VM_GO_ENGINE_PORT} -ge 60000 ] || [ ${VM_GO_ENGINE_PORT} -le 10000 ];then
-        echo "vm_go_engine_port should >=10000 && <=60000"
-        show_help
-        exit 1
-    fi
+    echo "param VM_GO_ENGINE_PORT $VM_GO_ENGINE_PORT"
 }
 
 function generate_certs() {
-    echo "begin generate certs, cnt: ${NODE_CNT}"
+    #echo "begin generate certs, cnt: ${NODE_CNT}"
     mkdir -p ${BUILD_PATH}
     cd "${BUILD_PATH}"
     if [ -d crypto-config ]; then
@@ -146,71 +169,126 @@ function generate_certs() {
 }
 
 function generate_config() {
-    LOG_LEVEL="INFO"
-    CONSENSUS_TYPE=1
+    LOG_LEVEL="" # default INFO
+    CONSENSUS_TYPE=0 # default  1
     MONITOR_PORT=14321
     PPROF_PORT=24321
     TRUSTED_PORT=13301
     VM_GO_CONTAINER_NAME_PREFIX="chainmaker-vm-go-container"
-    ENABLE_VM_GO="false"
-    VM_GO_LOG_LEVEL="INFO"
-    VM_GO_TRANSPORT_PROTOCOL="tcp" # tcp / uds
+    ENABLE_VM_GO="" # default false
+    VM_GO_LOG_LEVEL="" # default INFO
+    VM_GO_TRANSPORT_PROTOCOL="" # default tcp, uds
 
-    if  [ $NODE_CNT -gt 1 ] ;then
-      read -p "input consensus type (1-TBFT(default),3-MAXBFT,4-RAFT): " tmp
-      if  [ ! -z "$tmp" ] ;then
-        if [ $tmp -eq 1 ] || [ $tmp -eq 3 ] || [ $tmp -eq 4 ] ;then
-          CONSENSUS_TYPE=$tmp
-        else
-          echo "invalid consensus type [" $tmp "], so use default"
-        fi
-      fi
-    else
-      read -p "input consensus type (0-SOLO,1-TBFT(default),3-MAXBFT,4-RAFT): " tmp
-      if  [ ! -z "$tmp" ] ;then
-        if  [ $tmp -eq 0 ] || [ $tmp -eq 1 ] || [ $tmp -eq 3 ] || [ $tmp -eq 4 ] ;then
-          CONSENSUS_TYPE=$tmp
-        else
-          echo "unknown consensus type [" $tmp "], so use default"
-        fi
-      fi
-    fi
+    set -- $(getopt -u -o c:l:v: -l vtp:,vlog: "$@")   # -o 接收短参数， -l 接收长参数， 需要参数值的在参数后面添加:
+    while [ -n "$1" ]; do
+        case "$1" in
+            -c) CONSENSUS_TYPE=$2
+                 shift ;;
+            -l) LOG_LEVEL=$2
+                shift ;;
+            -v) ENABLE_VM_GO=$2
+                shift ;;
+            --vtp)
+                VM_GO_TRANSPORT_PROTOCOL=$2
+                shift ;;
+            --vlog)
+                VM_GO_LOG_LEVEL=$2
+                shift
+        esac
+        shift
+    done
 
-    read -p "input log level (DEBUG|INFO(default)|WARN|ERROR): " tmp
-    if  [ ! -z "$tmp" ] ;then
-      if  [ $tmp == "DEBUG" ] || [ $tmp == "INFO" ] || [ $tmp == "WARN" ] || [ $tmp == "ERROR" ];then
-          LOG_LEVEL=$tmp
-      else
-        echo "unknown log level [" $tmp "], so use default"
-      fi
-    fi
-
-    read -p "enable vm go (YES|NO(default))" enable_vm_go
-    if  [ ! -z "$enable_vm_go" ]; then
-      if  [ $enable_vm_go == "yes" ] || [ $enable_vm_go == "YES" ]; then
-          ENABLE_VM_GO="true"
-
-          read -p "vm go transport protocol (uds|tcp(default))" transport_protocol
-          if [ ! -z "$transport_protocol" ]; then
-              if [ $transport_protocol == "tcp" ] || [ $transport_protocol == "TCP" ]; then
-                  VM_GO_TRANSPORT_PROTOCOL="tcp"
-              elif [ $transport_protocol == "uds" ] || [ $transport_protocol == "UDS" ]; then
-                  VM_GO_TRANSPORT_PROTOCOL="uds"
-              else
-                  echo "unknown input [" $transport_protocol "], so use default"
-              fi
-          fi
-
-          read -p "input vm go log level (DEBUG|INFO(default)|WARN|ERROR): " vm_go_log_level
-          if  [ ! -z "$vm_go_log_level" ] ;then
-          if  [ $vm_go_log_level == "DEBUG" ] || [ $vm_go_log_level == "INFO" ] || [ $vm_go_log_level == "WARN" ] || [ $vm_go_log_level == "ERROR" ];then
-              VM_GO_LOG_LEVEL=$vm_go_log_level
+    # set CONSENSUS_TYPE
+    if [ $CONSENSUS_TYPE == 0 ] ;then
+      if  [ $NODE_CNT -gt 1 ] && [ "$CONSENSUS_TYPE" == "0" ] ;then
+        read -p "input consensus type (1-TBFT(default),3-MAXBFT,4-RAFT): " tmp
+        if  [ ! -z "$tmp" ] ;then
+          if [ $tmp -eq 1 ] || [ $tmp -eq 3 ] || [ $tmp -eq 4 ] ;then
+            CONSENSUS_TYPE=$tmp
           else
-              echo "unknown vm go log level [" $vm_go_log_level "], so use default"
+            echo "invalid consensus type [" $tmp "], so use default"
+          fi
+        fi
+      else
+        read -p "input consensus type (0-SOLO,1-TBFT(default),3-MAXBFT,4-RAFT): " tmp
+        if  [ ! -z "$tmp" ] ;then
+          if  [ $tmp -eq 0 ] || [ $tmp -eq 1 ] || [ $tmp -eq 3 ] || [ $tmp -eq 4 ] ;then
+            CONSENSUS_TYPE=$tmp
+          else
+            echo "unknown consensus type [" $tmp "], so use default"
           fi
         fi
       fi
     fi
+    if [ $CONSENSUS_TYPE == 0 ] ;then
+          CONSENSUS_TYPE=1
+    fi
+    echo "param CONSENSUS_TYPE $CONSENSUS_TYPE"
+
+    # set LOG_LEVEL
+    if [ "$LOG_LEVEL" == "" ] ;then
+      read -p "input log level (DEBUG|INFO(default)|WARN|ERROR): " tmp
+      if  [ ! -z "$tmp" ] ;then
+        if  [ $tmp == "DEBUG" ] || [ $tmp == "INFO" ] || [ $tmp == "WARN" ] || [ $tmp == "ERROR" ];then
+            LOG_LEVEL=$tmp
+        else
+          echo "unknown log level [" $tmp "], so use default"
+        fi
+      fi
+    fi
+    if [ "$LOG_LEVEL" == "" ] ;then
+        LOG_LEVEL="INFO"
+    fi
+    echo "param LOG_LEVEL $LOG_LEVEL"
+
+    # set ENABLE_VM_GO
+    if [ "$ENABLE_VM_GO" == "" ] ;then
+      read -p "enable vm go (YES|NO(default))" enable_vm_go
+      if  [ ! -z "$enable_vm_go" ]; then
+        if  [ $enable_vm_go == "yes" ] || [ $enable_vm_go == "YES" ]; then
+            ENABLE_VM_GO="true"
+
+            if [ "$VM_GO_TRANSPORT_PROTOCOL" == "" ] ;then
+              read -p "vm go transport protocol (uds|tcp(default))" transport_protocol
+              if [ ! -z "$transport_protocol" ]; then
+                  if [ $transport_protocol == "tcp" ] || [ $transport_protocol == "TCP" ]; then
+                      VM_GO_TRANSPORT_PROTOCOL="tcp"
+                  elif [ $transport_protocol == "uds" ] || [ $transport_protocol == "UDS" ]; then
+                      VM_GO_TRANSPORT_PROTOCOL="uds"
+                  else
+                      echo "unknown input [" $transport_protocol "], so use default"
+                  fi
+              fi
+            fi
+            if [ "$VM_GO_TRANSPORT_PROTOCOL" == "" ] ;then
+              VM_GO_TRANSPORT_PROTOCOL="tcp"
+            fi
+
+            if [ "$VM_GO_LOG_LEVEL" == "" ] ;then
+                read -p "input vm go log level (DEBUG|INFO(default)|WARN|ERROR): " vm_go_log_level
+                if  [ ! -z "$vm_go_log_level" ] ;then
+                if  [ $vm_go_log_level == "DEBUG" ] || [ $vm_go_log_level == "INFO" ] || [ $vm_go_log_level == "WARN" ] || [ $vm_go_log_level == "ERROR" ];then
+                    VM_GO_LOG_LEVEL=$vm_go_log_level
+                else
+                    echo "unknown vm go log level [" $vm_go_log_level "], so use default"
+                fi
+              fi
+            fi
+            if [ "$VM_GO_LOG_LEVEL" == "" ] ;then
+              VM_GO_LOG_LEVEL="INFO"
+            fi
+
+        fi
+      fi
+    fi
+    if [ "$ENABLE_VM_GO" == "" ] ;then
+      ENABLE_VM_GO="false"
+    elif [ $ENABLE_VM_GO == "true" ] ;then
+      echo "param VM_GO_LOG_LEVEL $VM_GO_LOG_LEVEL"
+      echo "param VM_GO_TRANSPORT_PROTOCOL $VM_GO_TRANSPORT_PROTOCOL"
+    fi
+    echo "param ENABLE_VM_GO $ENABLE_VM_GO"
+    echo
 
     cd "${BUILD_PATH}"
     if [ -d config ]; then
@@ -410,4 +488,4 @@ function generate_config() {
 
 check_params
 generate_certs
-generate_config
+generate_config $@
