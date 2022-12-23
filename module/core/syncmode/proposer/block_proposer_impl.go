f435dfba7e6db6c97c46c670a78ce1f04c6c7d0e
lzwisbadbad <lzwisbadbad@163.com>
2022-12-23 11:43:39 +0800

    fix: 修复提案状态未控制好导致的提案堵塞问题 --bug=1004805

--- module/core/syncmode/proposer/block_proposer_impl.go
+++ module/core/syncmode/proposer/block_proposer_impl.go
@@ -265,10 +265,7 @@ func (bp *BlockProposerImpl) proposeBlock() {
 // proposing, propose a block in new height
 func (bp *BlockProposerImpl) proposing(height uint64, preHash []byte) *commonpb.Block {
 	startTick := utils.CurrentTimeMillisSeconds()
-	//defer bp.yieldProposing()
-	defer func() {
-		bp.yieldProposing()
-	}()
+	defer bp.yieldProposing()
 
 	selfProposedBlock := bp.proposalCache.GetSelfProposedBlockAt(height)
 	if selfProposedBlock != nil {
