e4cfe0939bd9e1adef85ffcdf015c6834c19bf2c
liuxinfeng <liuxinfeng96@163.com>
2022-12-30 17:46:25 +0800

    fix: Fix stop the chain but net error	--bug=1004864

--- module/sync/blockchain_sync_server.go
+++ module/sync/blockchain_sync_server.go
@@ -552,8 +552,6 @@ func (sync *BlockChainSyncServer) validateAndCommitBlockWithRwSets(block *common
 func (sync *BlockChainSyncServer) StopBlockSync() {
 	_ = sync.scheduler.addTask(&StopSyncMsg{})
 	_ = sync.processor.addTask(&StopSyncMsg{})
-	sync.net.CancelSubscribe(netPb.NetMsg_SYNC_BLOCK_MSG)
-	sync.net.CancelReceiveMsg(netPb.NetMsg_SYNC_BLOCK_MSG)
 }
 
 //StartBlockSync make sync service resume sending sync block requests to other peer nodes
@@ -569,6 +567,8 @@ func (sync *BlockChainSyncServer) Stop() {
 	sync.scheduler.end()
 	sync.processor.end()
 	close(sync.close)
+	sync.net.CancelSubscribe(netPb.NetMsg_SYNC_BLOCK_MSG)
+	sync.net.CancelReceiveMsg(netPb.NetMsg_SYNC_BLOCK_MSG)
 }
 
 //OnMessage msgbus Subscriber interface implementation
