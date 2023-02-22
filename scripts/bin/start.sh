cbff51b74fdbc75b4b54cd6c0804ff40e31dbfd5
taifu <515789026@qq.com>
2023-02-22 10:31:50 +0800

    chore: update for v2.3.1 release --story=0

--- scripts/bin/start.sh
+++ scripts/bin/start.sh
@@ -33,7 +33,7 @@ config_file="../config/{org_id}/chainmaker.yml"
 eval $(parse_yaml "$config_file" "chainmaker_")
 
 VM_GO_IMAGE_NAME="chainmakerofficial/chainmaker-vm-engine:v2.3.1"
-DOCKER_VM_IMAGE_NAME="chainmakerofficial/chainmaker-vm-docker-go:v2.3.0.1"
+DOCKER_VM_IMAGE_NAME="chainmakerofficial/chainmaker-vm-docker-go:v2.3.1"
 START_FULL_MODE=""
 
 # read params
