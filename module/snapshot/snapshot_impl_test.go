eef4142d75f91157a5c48d74fb06f04826475bda
DevinZeng <studyzy@qq.com>
2023-02-22 16:34:32 +0800

    fix: Snapshot UT error
    --bug=1

--- module/snapshot/snapshot_impl_test.go
+++ module/snapshot/snapshot_impl_test.go
@@ -227,11 +227,6 @@ func (s *MockSimContextImpl) GetStrAddrFromPbMember(pbMember *acPb.Member) (stri
 	panic("impl me")
 }
 
-// GetSnapshot
-func (s *MockSimContextImpl) GetSnapshot() protocol.Snapshot {
-	panic("impl me")
-}
-
 func TestKey(t *testing.T) {
 	s0 := "你好"
 	b0 := []byte(s0)
