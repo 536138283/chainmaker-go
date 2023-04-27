930da6a74f3ecfaeb16429a355c8daaf04ac3c9b
hongdacai <1526875642@qq.com>
2023-04-27 11:13:46 +0800

    fix: add gas calculation into query tx. --bug=1005434

--- module/rpcserver/api_service.go
+++ module/rpcserver/api_service.go
@@ -199,8 +199,7 @@ func (s *ApiService) invoke(tx *commonPb.Transaction, source protocol.TxSource) 
 }
 
 // dealQuery - deal query tx
-// nolint: gocyclo
-// nolint: revive
+// nolint: revive, gocyclo
 func (s *ApiService) dealQuery(tx *commonPb.Transaction, source protocol.TxSource) *commonPb.TxResponse {
 	var (
 		err     error
