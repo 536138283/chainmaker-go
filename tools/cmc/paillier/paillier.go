1e0a9a9bf5551ce956f901ea4374b83a90047642
wanghongda <harryhdwang@tencent.com>
2023-08-09 14:49:31 +0800

    feat: add paillier encrypt/decrypt cmd --story=1013637

--- tools/cmc/paillier/paillier.go
+++ tools/cmc/paillier/paillier.go
@@ -7,20 +7,28 @@ SPDX-License-Identifier: Apache-2.0
 package paillier
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"os"
 	"path/filepath"
+	"strconv"
 
+	"chainmaker.org/chainmaker-go/tools/cmc/util"
 	"chainmaker.org/chainmaker/common/v2/crypto/paillier"
 	"github.com/spf13/cobra"
 )
 
 var (
-	// genKeyCmd flags
+	// genKeyCmd flag vars
 	paillierKeySavePath string
 	paillierKeyFileName string
+
+	paillierPrivKeyFilePath string
+	paillierPubKeyFilePath  string
+	paillierData            string
 )
 
 // PaillierCMD new ChainMaker paillier command
@@ -32,6 +40,8 @@ func PaillierCMD() *cobra.Command {
 	}
 
 	paillierCmd.AddCommand(genKeyCMD())
+	paillierCmd.AddCommand(encryptCMD())
+	paillierCmd.AddCommand(decryptCMD())
 	return paillierCmd
 }
 
@@ -48,7 +58,8 @@ func genKeyCMD() *cobra.Command {
 	}
 
 	flags := genKeyCmd.Flags()
-	flags.StringVarP(&paillierKeySavePath, "path", "", "", "the result storage file path, and the file name is the id")
+	flags.StringVarP(&paillierKeySavePath, "path", "", "",
+		"the result storage file path, and the file name is the id")
 	flags.StringVarP(&paillierKeyFileName, "name", "", "", "")
 
 	return genKeyCmd
@@ -124,4 +135,102 @@ func pathExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+// encryptCMD encrypt user data for paillier
+// @return *cobra.Command
+func encryptCMD() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "encrypt",
+		Short: "use paillier public key encrypt user data, the output encrypted data is in base64 format",
+		Long:  "use paillier public key encrypt user data, the output encrypted data is in base64 format",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			pubKeyBytes, err := ioutil.ReadFile(paillierPubKeyFilePath)
+			if err != nil {
+				return err
+			}
+
+			pubKey := new(paillier.PubKey)
+			err = pubKey.Unmarshal(pubKeyBytes)
+			if err != nil {
+				return err
+			}
+
+			inData, err := strconv.ParseInt(paillierData, 10, 64)
+			if err != nil {
+				return err
+			}
+			cipherData, err := pubKey.Encrypt(new(big.Int).SetInt64(inData))
+			if err != nil {
+				return err
+			}
+			cipherBytes, err := cipherData.Marshal()
+			if err != nil {
+				return err
+			}
+			cipherStr := base64.StdEncoding.EncodeToString(cipherBytes)
+
+			util.PrintPrettyJson(map[string]interface{}{
+				"result": cipherStr,
+			})
+			return nil
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&paillierPubKeyFilePath, "pubkey-file-path", "",
+		"specify paillier public key file path")
+	flags.StringVar(&paillierData, "data", "",
+		"specify input data for encrypt. e.g. --data=\"123\"")
+
+	return cmd
+}
+
+// decryptCMD decrypt user data for paillier
+// @return *cobra.Command
+func decryptCMD() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "decrypt",
+		Short: "use paillier private key decrypt user data, the input encrypted data must in base64 format",
+		Long:  "use paillier private key decrypt user data, the input encrypted data must in base64 format",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			prvKeyBytes, err := ioutil.ReadFile(paillierPrivKeyFilePath)
+			if err != nil {
+				return err
+			}
+			prvKey := new(paillier.PrvKey)
+			err = prvKey.Unmarshal(prvKeyBytes)
+			if err != nil {
+				return err
+			}
+
+			ct := new(paillier.Ciphertext)
+			inData, err := base64.StdEncoding.DecodeString(paillierData)
+			if err != nil {
+				return err
+			}
+			err = ct.Unmarshal(inData)
+			if err != nil {
+				return err
+			}
+
+			decrypt, err := prvKey.Decrypt(ct)
+			if err != nil {
+				return err
+			}
+
+			util.PrintPrettyJson(map[string]interface{}{
+				"result": decrypt.Int64(),
+			})
+			return nil
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&paillierPrivKeyFilePath, "privkey-file-path", "",
+		"specify paillier private key file path")
+	flags.StringVar(&paillierData, "data", "",
+		"specify input data for decrypt. e.g. --data=\"some base64 string\"")
+
+	return cmd
 }
