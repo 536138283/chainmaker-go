98c6d26d127391df14635e5971abc650a3ad5d36
weiningzhou <269475239@qq.com>
2024-02-29 15:55:14 +0800

    fix: ci qta
    --bug=1

--- scripts/docker/build-dockergo.sh
+++ scripts/docker/build-dockergo.sh
@@ -9,7 +9,7 @@ function buildImage() {
   rm -rf vm-engine
   git clone https://git.code.tencent.com/ChainMaker/vm-engine.git
   cd vm-engine
-  git checkout v2.3.5
+  git checkout v2.3.3_qc
   make build-image
   cd ..
   rm -rf vm-engine
