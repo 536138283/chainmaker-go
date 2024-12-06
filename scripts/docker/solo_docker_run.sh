963d6197d0d30da3fb519a7a15a7d5162675d1a7
shaozhuguang <leonzgshao@tencent.com>
2024-12-06 15:27:07 +0800

    chore: modify version to v2.3.6.1 --story=0

--- scripts/docker/solo_docker_run.sh
+++ scripts/docker/solo_docker_run.sh
@@ -16,7 +16,7 @@ docker run   \
  -v ./tmp_solo/log:/chainmaker-go/log \
  --privileged=true  \
  --name csolo  \
- chainmakerofficial/chainmaker:v2.3.5 \
+ chainmakerofficial/chainmaker:v2.3.6.1 \
  bash -c "./chainmaker start -c ../config/wx-org1.chainmaker.org/chainmaker.yml > panic.log"
 
 
