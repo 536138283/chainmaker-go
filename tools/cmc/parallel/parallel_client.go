e3e1cd79b2f029e4cd3bd6ba75fc4db84cfb3f24
tiantian <2381385438@qq.com>
2025-04-15 14:49:52 +0800

    feat: update every client conn count to 10 --story=0

--- tools/cmc/parallel/parallel_client.go
+++ tools/cmc/parallel/parallel_client.go
@@ -23,7 +23,7 @@ func getSdkClient(i int) (*sdk.ChainClient, error) {
 		// 节点地址，格式：127.0.0.1:12301
 		sdk.WithNodeAddr(hosts[i]),
 		// 节点连接数
-		sdk.WithNodeConnCnt(threadNum*len(hosts)),
+		sdk.WithNodeConnCnt(10),
 		// 节点是否启用TLS认证
 		sdk.WithNodeUseTLS(useTLS),
 		// 根证书路径，支持多个
