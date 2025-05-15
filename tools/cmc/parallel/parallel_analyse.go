04784029898e3c438be3451885741ea6e6c869fb
tiantian <2381385438@qq.com>
2025-05-15 18:24:25 +0800

    fix: block only print node info --bug=0

--- tools/cmc/parallel/parallel_analyse.go
+++ tools/cmc/parallel/parallel_analyse.go
@@ -244,9 +244,6 @@ func printChainDetail(s *Statistician) error {
 	chainResult := &ChainResultSet{}
 	s.outNodeBlockInfo(chainResult)
 	// 如果没有产出区块则不记录统计直接返回
-	if chainResult.BlockNum == 0 {
-		return nil
-	}
 	jsonByte, err := json.Marshal(chainResult)
 	if err != nil {
 		fmt.Println("marshal chain result error:", err)
